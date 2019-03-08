2743e9a7252f85afd2758ef735c3346188525f7f
Valentin Vetter <vetter.valentin@gmail.com>
2019-03-08 23:06:08 +0100

    chore: make TS more strict

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as Phaser from 'phaser'
 import GameScene from './GameScene'
 
+declare global {
+  interface Window {
+    game: Phaser.Game
+  }
+}
+
 const config: GameConfig = {
   title: 'Bejeweled',
   width: 520,
@@ -12,9 +18,3 @@ const config: GameConfig = {
 const game = new Phaser.Game(config)
 
 window.game = game
-
-declare global {
-  interface Window {
-    game: Phaser.Game
-  }
-}
